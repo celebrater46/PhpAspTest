6495f42985498b90950a0514e4c55a731388fdad
hideru <jota.16.stigma.world@gmail.com>
2021-06-26 15:44:11 +0900

    add get, get_asp, result

--- get_asp.php
+++ get_asp.php
@@ -1,10 +1,19 @@
 <?php
 
-// Refer: https://qiita.com/fantm21/items/603cbabf2e78cb08133e
+// Refer: https://qiita.com/fantm21/items/603cbabf2e78cb08133e JSONの取得
+// Refer: https://qiita.com/izanari/items/f4f96e11a2b01af72846 SSLエラーについて
 
-$json = file_get_contents($url);
+$url = "https://localhost:5001/weatherforecast";
+
+$options['ssl']['verify_peer']=false;
+$options['ssl']['verify_peer_name']=false;
+
+//$json = file_get_contents($url);
+$json = file_get_contents($url, false, stream_context_create($options));
 $json = mb_convert_encoding($json, 'UTF8', 'ASCII,JIS,UTF-8,EUC-JP,SJIS-WIN');
 $arr = json_decode($json,true);
 
-echo $arr->res->blogData->id;
-echo $arr['res']['blogData']['id'];
+//echo $arr->res->blogData->id;
+//echo $arr['res']['blogData']['id'];
+
+var_dump($arr);
